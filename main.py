eaf901cdd72ca030a6fe7ce49f72a899ac379366
SaeedZahran <saeedzahran@hotmail.com>
2022-09-28 11:37:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,13 +30,16 @@ raw = mne.io.read_raw_fif(fname, verbose=False)
 # eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, n_grad=1, n_mag=1,
 #                                                          n_eeg=1, average=True)
 
-eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, None, config['tmin'], config['tmax'],
-                                   config['n_grad'], config['n_mag'], config['n_eeg'], config['l_freq'],
-                                   config['h_freq'], config['average'], config['filter_length'], None,
-                                   None, None, [], config['avg_ref'], config['no_proj'], config['event_id'],
-                                   config['eog_l_freq'], config['eog_h_freq'], config['tstart'],
-                                   config['filter_method'], None, None, config['copy'],
-                                   config['return_drop_log'], config['meg'])
+eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, n_grad=config['n_grad'], n_mag=config['n_mag'],
+                                                         n_eeg=config['n_eeg'], average=True)
+
+# eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, None, config['tmin'], config['tmax'],
+#                                    config['n_grad'], config['n_mag'], config['n_eeg'], config['l_freq'],
+#                                    config['h_freq'], config['average'], config['filter_length'], None,
+#                                    None, None, [], config['avg_ref'], config['no_proj'], config['event_id'],
+#                                    config['eog_l_freq'], config['eog_h_freq'], config['tstart'],
+#                                    config['filter_method'], None, None, config['copy'],
+#                                    config['return_drop_log'], config['meg'])
 
 # eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, None, config['tmin'], config['tmax'], config['n_grad'],
 #             config['n_mag'], config['n_eeg'], config['l_freq'], config['h_freq'], config['average'], config['filter_length'], None, None, None, None, [],
