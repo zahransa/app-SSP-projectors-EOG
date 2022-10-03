546cf4aea55f175a05c25c4e44d3b63618cd62c6
SaeedZahran <saeedzahran@hotmail.com>
2022-10-03 07:20:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,12 @@ raw = mne.io.read_raw_fif(fname, verbose=False)
 eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, n_grad=config['n_grad'], n_mag=config['n_mag'],
                                                          n_eeg=config['n_eeg'], average=config['average'])
 
+# == FIGURES ==
+plt.figure(1)
+fig_ep = mne.viz.plot_projs_topomap(eog_projs, info=raw.info)
+fig_ep.savefig(os.path.join('out_figs','eog_projectors.png'))
+
+
 # eog_projs, eog_events = mne.preprocessing.compute_proj_eog(raw, None, config['tmin'], config['tmax'],
 #                                    config['n_grad'], config['n_mag'], config['n_eeg'], config['l_freq'],
 #                                    config['h_freq'], config['average'], config['filter_length'], None,
